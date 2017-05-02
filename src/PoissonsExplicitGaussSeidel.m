b0a453815be56545bcdd70d9e9684c2b4e532341
nidalch <nidalchk@hotmail.com>
2017-05-02 13:13:31 -0500

    7th working attempt

--- src/PoissonsExplicitGaussSeidel.m
+++ src/PoissonsExplicitGaussSeidel.m
@@ -4,8 +4,8 @@
 clc
 clear all
 close all
-x=1000;
-y=1000;
+x=50;
+y=50;
 u=zeros(x,y);
 bx=pi;
 ax=-pi;
@@ -15,26 +15,26 @@ xd=linspace(ax,bx,x);
 yd=linspace(ay,by,y);
 ni=100
 h=ax/x;
-for k=1:ni
-for i=2:x-1
+
+ u(1,:)=((by-yd(:)).^2).*cos(pi.*yd(:)/by);
+ u(x,:)=yd(:).*(by-yd(:)).^2;
+ u(:,y)=yd(:);
+ u(:,1)=(((by-ay).^2).*cos(pi.*ay/by))+((xd(:)-ax)/(bx-ax)).*((ay.*((by-ay).^2)-((by-ay).^2).*cos(pi*ay/by)));
+
+        
+    for k=1:ni
+    for i=2:x-1
     for j=2:y-1
         %F(i,j)=0;
         F(i,j)=cos((pi/2)*(2*((xd(i)-ax)/(bx-ax))+1))*sin(pi*(yd(j)-ay)/(by-ay));
         u(i,j)=(1/4)*(u(i+1,j)+u(i-1,j)+u(i,j+1)+u(i,j-1)+(h.^2)*F(i,j));
         
-        u(1,j)=((by-yd(j)).^2)*cos(pi*yd(j)/by);
-        u(x,j)=yd(j)*(by-yd(j)).^2;
-        u(i,y)=yd(j);
-        u(i,1)=((by-ay).^2)*cos(pi*ay/by)+((xd(i)-ax)/(bx-ax))*((ay*((by-ay).^2)-((by-ay).^2)*cos(pi*ay/by)));
-       
     end
+    u(i,y
 end
 end
 %Inital BC
- u(1,1)=((by-yd(1)).^2)*cos(pi*yd(1)/by);
- u(x,1)=yd(1)*(by-yd(1)).^2;
- u(x,1)=yd(1)*(by-yd(1)).^2;
-%Keep messing with BCs and grid sizes-table to create
+
 
 [X,Y]=meshgrid(xd,yd);
 figure(1)
